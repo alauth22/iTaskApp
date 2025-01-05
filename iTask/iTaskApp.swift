0357513ca1270e2a9bfc41573a2261a9261c98b5
alauth22 <amelialauth@gmail.com>
2025-01-05 17:54:25 -0600

    Initial Commit

---
+++ iTask/iTaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  iTaskApp.swift
+//  iTask
+//
+//  Created by Amelia Lauth on 1/5/25.
+//
+
+import SwiftUI
+
+@main
+struct iTaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ iTaskTests/iTaskTests.swift
@@ -0,0 +1,17 @@
+//
+//  iTaskTests.swift
+//  iTaskTests
+//
+//  Created by Amelia Lauth on 1/5/25.
+//
+
+import Testing
+@testable import iTask
+
+struct iTaskTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
